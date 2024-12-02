16cb9ad4681c8447025bbff75ef0581a9a34f438
TheoDegorre <degorret@gmail.com>
2024-12-02 22:53:48 +0100

    responsive eventpage

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,28 +1,49 @@
 import EventCard from "../components/EventCard";
 import eventdata from "../eventdata";
-import background from "../assets/Vector.svg";
+
 function EventPage() {
+  function formatDate(dateString) {
+    const [day, month, year] = dateString.split("/");
+    const date = new Date(`${year}-${month}-${day}`);
+    return date;
+  }
+
+  function formatDateToString(dateString) {
+    const date = formatDate(dateString);
+    const formatter = new Intl.DateTimeFormat("fr-FR", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+
+    return formatter.format(date);
+  }
+
   return (
     <>
       <main className="event-page-container">
-        <div id="event-background-container">
-          <img id="background" src={background} alt="" />
-        </div>
-        <h1>Dates à venir :</h1>
-        <div className="line" />
+
+        <hgroup className="event-page-title">
+          <h1>Dates à venir :</h1>
+          <div className="line" />
+        </hgroup>
+
         <section className="event-list-container">
-          {eventdata.map((el) => (
-            <EventCard
-              key={el.date}
-              id={el.id}
-              date={el.date}
-              title={el.title}
-              place={el.place}
-              adress={el.adress}
-              city={el.city}
-            />
-          ))}
+          {eventdata
+            .sort((a, b) => formatDate(b.date) - formatDate(a.date))
+            .map((el) => (
+              <EventCard
+                key={el}
+                id={el.id}
+                date={formatDateToString(el.date)}
+                title={el.title}
+                place={el.place}
+                adress={el.adress}
+                city={el.city}
+              />
+            ))}
         </section>
+        
       </main>
     </>
   );
