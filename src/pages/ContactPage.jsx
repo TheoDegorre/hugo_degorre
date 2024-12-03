46b929dca8c4ba36268ccfdf11886eab06a23676
TheoDegorre <degorret@gmail.com>
2024-12-03 01:12:38 +0100

    responsive contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,30 +1,24 @@
-import contact from "../assets/contact_page/contact.jpg"
+import contact from "../assets/contact_page/hugo_contact.webp";
 
 function ContactPage() {
   return (
     <section className="contact-page-container">
-
       <figure className="contact-figure">
-        <img
-          id="figure-img"
-          src={contact}
-          alt=""
-        />
+        <img id="figure-img" src={contact} alt="" />
+        <div id="contact-blur" />
       </figure>
-<figcaption className="contact-page-figcaption">
 
+      <figcaption className="contact-figcaption">
         <a href="mailto:degorreh@gmail.com">degorreh@gmail.com</a>
-        <div id="contact-line"/>
-      
-      <div className="footer-social-link">
-        <a href="http://google.fr" className="fa fa-facebook" />
-        <a href="http://google.fr" className="fa fa-soundcloud" />
-        <a href="http://google.fr" className="fa fa-instagram" />
-      </div>
-      
+        <div id="contact-line" />
 
-</figcaption>
-      <div id="contact-blur" />
+        <div className="footer-social-link">
+          <a href="http://google.fr" className="fa fa-facebook" />
+          <a href="http://google.fr" className="fa fa-soundcloud" />
+          <a href="http://google.fr" className="fa fa-instagram" />
+        </div>
+      </figcaption>
+
     </section>
   );
 }
